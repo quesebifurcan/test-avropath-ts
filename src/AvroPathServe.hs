b4463981e3d8a72fa052b207cd8f08797c259b33
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2018-05-26 21:16:31 +0200

    roundtrip test

--- src/AvroPathServe.hs
+++ src/AvroPathServe.hs
@@ -1,24 +1,39 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Spock
 import Web.Spock.Config
 
-import Control.Monad.Trans
+import Data.Aeson hiding (json)
 import Data.Monoid
-import Data.IORef
-import qualified Data.Text as T
+import Data.Text (Text, pack)
+import GHC.Generics
 
 -- import AvroPath (getTsTypeName, TsType(..))
 
-data MySession = EmptySession
-data MyAppState = DummyAppState (IORef Int)
+type Api = SpockM () () () ()
+
+type ApiAction a = SpockAction () () () a
+
+data Test = Test { a :: Int }
+  deriving (Generic, Show)
+
+instance ToJSON Test
+instance FromJSON Test
 
 main :: IO ()
-main =
-    do ref <- newIORef 0
-       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
-       runSpock 8080 (spock spockCfg app)
+main = do
+  spockCfg <- defaultSpockCfg () PCNoDatabase ()
+  runSpock 8080 (spock spockCfg app)
 
-app :: SpockM () MySession MyAppState ()
-app =
-    do get root $ text "testing" 
+app :: Api
+app = do
+  get "test" $ do
+    json (Test 123)
+  post "test" $ do
+    test <- jsonBody' :: ApiAction Test
+    json (result test)
+    where
+      result x = Test { a = (a x) + 1 }
+
+
